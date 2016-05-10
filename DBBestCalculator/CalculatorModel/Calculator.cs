b7dd1959b02c63aa5c0ff13a01bc0201c435946a
Liahim78 <aweerr78@gmail.com>
2016-05-10 17:51:42 +0300

    Calculator add

--- DBBestCalculator/CalculatorModel/Calculator.cs
+++ DBBestCalculator/CalculatorModel/Calculator.cs
@@ -15,6 +15,9 @@ namespace CalculatorModel
         public string Operation { get; set; }
         public string Result { get { return result; } }
 
+
+        public bool newDisplayRequired { get; set; }
+
         public Calculator()
         {
             FirstOperand = string.Empty;
@@ -93,6 +96,98 @@ namespace CalculatorModel
                     result = "Unknown operation: " + operation;
                     throw new ArgumentException ("Unknown operation: "+ operation, "operation");
             }
+        }
+        /// <summary>
+        /// a method that is executed by pressing the button
+        /// </summary>
+        /// <param name="button"></param>
+        public void DigitButtonPress(string button, ref string Display, ref string LastOperation, ref string FullExpression)
+        {
+            switch (button)
+            {
+                case "C":
+                    Display = "0";
+                    FirstOperand = string.Empty;
+                    SecondOperand = string.Empty;
+                    Operation = string.Empty;
+                    LastOperation = string.Empty;
+                    FullExpression = string.Empty;
+                    break;
+                case "Del":
+                    if (Display.Length > 1)
+                        Display = Display.Substring(0, Display.Length - 1);
+                    else Display = "0";
+                    break;
+                case "+/-":
+                    if (Display.Contains("-") || Display == "0")
+                    {
+                        Display = Display.Remove(Display.IndexOf("-"), 1);
+                    }
+                    else Display = "-" + Display;
+                    break;
+                case ",":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0,";
+                    }
+                    else
+                    {
+                        if (!Display.Contains(","))
+                        {
+                            Display = Display + ",";
+                        }
+                    }
+                    break;
+                default:
+                    if (Display == "0" || newDisplayRequired)
+                        Display = button;
+                    else
+                        Display = Display + button;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+        /// <summary>
+        /// a method that is executed by pressing the button operation
+        /// </summary>
+        /// <param name="operation"></param>
+        public void OperationButtonPress(string operation, ref string Display, ref string LastOperation, ref string FullExpression)
+        {
+            try
+            {
+                if (FirstOperand == string.Empty || LastOperation == "=")
+                {
+                    FirstOperand = Display;
+                    LastOperation = operation;
+                }
+                else
+                {
+                    SecondOperand = Display;
+                    Operation = LastOperation;
+                    CalculateResult();
+
+                    FullExpression = Math.Round(Convert.ToDouble(FirstOperand), 10) + " " + Operation + " "
+                                    + Math.Round(Convert.ToDouble(SecondOperand), 10) + " = "
+                                    + Math.Round(Convert.ToDouble(Result), 10);
+
+                    LastOperation = operation;
+                    Display = Result;
+                    FirstOperand = Display;
+
+                }
+            }
+            catch (Exception)
+            {
+                Display = Result == string.Empty ? "Error - see event log" : Result;
+                // reset values
+                this.FirstOperand = string.Empty;
+                this.SecondOperand = string.Empty;
+                this.Operation = string.Empty;
+                LastOperation = string.Empty;
+                FullExpression = string.Empty;
+                FullExpression = "";
+            }
+            newDisplayRequired = true;
         }
     }
 }
--- DBBestCalculator/DBBestCalculator/ViewModels/CalculatorViewModel.cs
+++ DBBestCalculator/DBBestCalculator/ViewModels/CalculatorViewModel.cs
@@ -18,27 +18,8 @@ namespace DBBestCalculator.ViewModels
         private DelegateCommand<string> operationButtonPressCommand;
 
         private string lastOperation;
-        private bool newDisplayRequired = false;
         private string display;
         private string fullExpression;
-
-        public string FirstOperand
-        {
-            get { return calculation.FirstOperand; }
-            set { calculation.FirstOperand = value; }
-        }
-
-        public string SecondOperand
-        {
-            get { return calculation.SecondOperand; }
-            set { calculation.SecondOperand = value; }
-        }
-
-        public string Operation
-        {
-            get { return calculation.Operation; }
-            set { calculation.Operation = value; }
-        }
 
         public string LastOperation
         {
@@ -58,19 +39,16 @@ namespace DBBestCalculator.ViewModels
         public string Display
         {
             get { return display; }
-            set{ display = value; OnPropertyChanged("Display");}
+            set { display = value; OnPropertyChanged("Display"); }
         }
         #endregion
 
         public CalculatorViewModel()
         {
             this.calculation = new Calculator();
-            this.display = "0";
-            this.FirstOperand = string.Empty;
-            this.SecondOperand = string.Empty;
-            this.Operation = string.Empty;
-            this.lastOperation = string.Empty;
-            this.fullExpression = string.Empty;
+            this.Display = "0";
+            this.LastOperation = string.Empty;
+            this.FullExpression = string.Empty;
         }
 
         public ICommand OperationButtonPressCommand
@@ -80,7 +58,8 @@ namespace DBBestCalculator.ViewModels
                 if (operationButtonPressCommand == null)
                 {
                     operationButtonPressCommand = new DelegateCommand<string>(
-                        OperationButtonPress, (string number) => { return true; });
+                       OperationButtonPress, (string number) => { return true; });
+                    
                 }
                 return operationButtonPressCommand;
             }
@@ -98,56 +77,6 @@ namespace DBBestCalculator.ViewModels
                 return digitButtonPressCommand;
             }
         }
-        /// <summary>
-        /// a method that is executed by pressing the button
-        /// </summary>
-        /// <param name="button"></param>
-        public void DigitButtonPress(string button)
-        {
-            switch (button)
-            {
-                case "C":
-                    Display = "0";
-                    FirstOperand = string.Empty;
-                    SecondOperand = string.Empty;
-                    Operation = string.Empty;
-                    LastOperation = string.Empty;
-                    FullExpression = string.Empty;
-                    break;
-                case "Del":
-                    if (display.Length > 1)
-                        Display = display.Substring(0, display.Length - 1);
-                    else Display = "0";
-                    break;
-                case "+/-":
-                    if (display.Contains("-") || display == "0")
-                    {
-                        Display = display.Remove(display.IndexOf("-"), 1);
-                    }
-                    else Display = "-" + display;
-                    break;
-                case ",":
-                    if (newDisplayRequired)
-                    {
-                        Display = "0,";
-                    }
-                    else
-                    {
-                        if (!display.Contains(","))
-                        {
-                            Display = display + ",";
-                        }
-                    }
-                    break;
-                default:
-                    if (display == "0" || newDisplayRequired)
-                        Display = button;
-                    else
-                        Display = display + button;
-                    break;
-            }
-            newDisplayRequired = false;
-        }
 
         /// <summary>
         /// a method that is executed by pressing the button operation
@@ -155,41 +84,16 @@ namespace DBBestCalculator.ViewModels
         /// <param name="operation"></param>
         public void OperationButtonPress(string operation)
         {
-            try
-            {
-                if (FirstOperand == string.Empty || LastOperation == "=")
-                {
-                    FirstOperand = display;
-                    LastOperation = operation;
-                }
-                else
-                {
-                    SecondOperand = display;
-                    Operation = lastOperation;
-                    calculation.CalculateResult();
+            calculation.OperationButtonPress(operation, ref display, ref lastOperation, ref fullExpression);
+            Display = display;
+            FullExpression = fullExpression;
+        }
 
-                    FullExpression = Math.Round(Convert.ToDouble(FirstOperand), 10) + " " + Operation + " "
-                                    + Math.Round(Convert.ToDouble(SecondOperand), 10) + " = "
-                                    + Math.Round(Convert.ToDouble(Result), 10);
-
-                    LastOperation = operation;
-                    Display = Result;
-                    FirstOperand = display;
-                    
-                }
-            }
-            catch (Exception)
-            {
-                Display = Result == string.Empty ? "Error - see event log" : Result;
-                // reset values
-                this.FirstOperand = string.Empty;
-                this.SecondOperand = string.Empty;
-                this.Operation = string.Empty;
-                this.lastOperation = string.Empty;
-                this.fullExpression = string.Empty;
-                FullExpression = "";
-            }
-            newDisplayRequired = true;
+        public void DigitButtonPress(string operation)
+        {
+            calculation.DigitButtonPress(operation, ref display, ref lastOperation, ref fullExpression);
+            Display = display;
+            FullExpression = fullExpression;
         }
     }
 }
